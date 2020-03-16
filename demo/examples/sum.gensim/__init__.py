785fd3363ac581119071b75fcc7655a182090208
Niyaz <niyazm524@gmail.com>
2020-03-16 21:02:17 +0300

    sum.gensim added to examples

--- demo/examples/sum.gensim/__init__.py
+++ demo/examples/sum.gensim/__init__.py
@@ -1,23 +1,16 @@
 """ docstrings """
 
 """
-NOTE: Здесь описываем базово основные методы с параметрами
-В вольном формате =)
+Есть только один метод, конфигурация передаётся через параметры этого метода
 """
 
 """
 Метод для сокращения текста
 :method 
-:name lib/summ/summarise
-:param required*: {str} Какой-нибудь обязательный параметр
-:param optional: {str} Какой-нибудь опциональный параметр
+:name gensim.summarization.summarizer.summarize
+:param required*: {str} text - текст, который нужно сократить
+:param optional: {Float} ratio - от 0 до 1, насколько нужно сокращать текст
+:param optional: {int} word_count - количество слов на выходе. Имеет приоритет перед ratio.
+:param optional: {Bool} split - возвращать список предложений или сразу соединенные предложения
 :return: {str} возвращает сокращенный текст
-"""
-
-"""
-Метод для конфигурации модуля
-:void
-:name lib/summ/config
-:param required*: {str} Какой-нибудь обязательный параметр
-:param optional: {int} Какой-нибудь опциональный параметр
 """
