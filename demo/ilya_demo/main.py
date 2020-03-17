0d7ce6d6597f4364fe00dc9e09f8e03947bc1f98
Ilya Azin <martis.azin@gmail.com>
2020-03-17 11:11:34 +0300

    add draft for demo

---
+++ demo/ilya_demo/main.py
@@ -0,0 +1,32 @@
+"""
+Точка входа в программу
+- высчитывает выборку по сокращенным предложениям
+- токенизирует (еще раз) самостоятельно текст по предложениям
+- отображает результат согласно вычисленным данным
+"""
+from core import (
+    create_total_selection,
+    fonts,
+    impls,
+    tokenize_sentences
+)
+
+priorities = [
+    '',
+    fonts.yellow,
+    fonts.red,
+    fonts.red2
+]
+
+with open('./fixtures.txt', 'r', encoding='utf-8') as fixture:
+    # compute total selection
+    text = fixture.read()
+    selection = create_total_selection(text, impls)
+    text_sentences = tokenize_sentences(text)
+    # print results
+    for i, sentence in enumerate(text_sentences):
+        weight = selection[i]
+        priority = priorities[weight]
+        # print(f'{i} [{weight}] {priority}{sentence}{fonts.end}')
+        print(f'{priority}{sentence}{fonts.end}')
+    print(selection, len(selection))
