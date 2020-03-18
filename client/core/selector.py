dea4d73cf4e31ffe5ac88654eb4f5668aac049ec
Ilya Azin <martis.azin@gmail.com>
2020-03-18 22:28:04 +0300

    fix compare logic

--- client/core/selector.py
+++ client/core/selector.py
@@ -14,19 +14,18 @@ def create_total_selection(text: str, impls) -> List[int]:
     return list(total_selection)
 
 
-# FIXME: selector & tokenizer on news.covid19
 def create_selection(text: str, summary: str) -> List[int]:
-    """ Просчет единичной выборки """
-    # FIXME: aka merge sort? (iter by text_sentences)
+    """
+    Просчет единичной выборки
+    :remark Проверяем наличие каждого предложения из текста в выдержке
+    Ввиду того, что выдержка может быть изменена
+    :fixme [...}.]["{...]
+    """
     from .tokenizer import tokenize_sentences
-    # токенизируем текст и выдержку
     text_sentences = tokenize_sentences(text)
-    sum_sentences = tokenize_sentences(summary)
-    # пробегаемся по тексту, а не по выдержке,
-    # т.к. в выдержке предложения потенциально могут быть изменены
     selection = [0] * len(text_sentences)
     for i, ts in enumerate(text_sentences):
-        relevant_sentences = [ss for ss in sum_sentences if ts.__contains__(ss) or ss.__contains__(ts)]
+        relevant_sentences = summary.__contains__(ts)
         if relevant_sentences:
             selection[i] += 1
     return selection
