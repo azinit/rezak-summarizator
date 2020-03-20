3e8a92c5f01a8c047beb8f9dd526002e09d5a5f0
Ilya Azin <martis.azin@gmail.com>
2020-03-20 23:47:14 +0300

    add bootstrap

--- client/src/components/app/index.tsx
+++ client/src/components/app/index.tsx
@@ -1,29 +1,40 @@
 import React from 'react'
+import { Card, CardGroup, CardDeck, CardColumns } from 'react-bootstrap'
 import "./index.scss";
-
 // TODO: add permissions to page
 // TODO: add popup on page
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='rezak-app'>
-        <section>
-          <h1 className='title'>Rezak plugin</h1>
-          <p>Chrome-Extension для сокращения текста</p>
-        </section>
-        <section>
-          <h2>Содержит реализации</h2>
-          <ul>
-            <li>gensim</li>
-            <li>pytldr</li>
-            <li>sumy</li>
-            <li>summy</li>
-            <li>textrank</li>
-          </ul>
-        </section>
-      </div>
-    )
-  }
+const App = () => {
+  const impls = [
+    'gensim',
+    'pytldr',
+    'sumy',
+    'summa',
+    'textrank'
+  ]
+  return (
+    <div className='rezak-app'>
+      <section>
+        <h1 className='title'>Rezak plugin</h1>
+        <p>Chrome-Extension для сокращения текста</p>
+      </section>
+      <section>
+        <h2>Содержит реализации</h2>
+        <CardGroup className='list-unstyled flex'>
+          {impls.map((impl: string) => (
+            <li>
+              <Card className='border-0 mb-1'>
+                <Card.Body className='bg-dark text-secondary'>
+                  <Card.Title>
+                    &gt; {impl}
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            </li>
+          ))}
+        </CardGroup>
+      </section>
+    </div>
+  )
 }
 export default App
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app";
+import './index.scss'
 
 ReactDOM.render(
     <App />, document.querySelector("#root")
