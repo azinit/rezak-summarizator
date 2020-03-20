ceac0a1335d9cef4445fccda22bbf8b90ef17a76
niyazm524 <niyazm524@gmail.com>
2020-03-20 20:50:50 +0300

    added server prototype with /reduce

---
+++ prototype_server/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, escape, request
+from random import random
+from core import (
+    create_total_selection,
+    colors,
+    tokenize_sentences
+)
+
+app = Flask(__name__)
+
+
+@app.route('/reduce', methods=['POST'])
+def reduce():
+    content = request.get_json()
+    if not content.text:
+        raise AttributeError('Please provide text parameter')
+    options = content.sum_options if content.sum_options else {"ratio": 0.5}
+    text_sentences = tokenize_sentences(content.text)
+    selection = create_total_selection(text_sentences, options)
+    return {"text_sentences": text_sentences, "total_selection": selection}
