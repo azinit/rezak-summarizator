7a1365a453e70fd0c6b10a9d5fd3f2fd95864269
niyazm524 <niyazm524@gmail.com>
2020-03-21 23:17:38 +0300

    python errors fixed

--- core/__init__.py
+++ core/__init__.py
@@ -1,3 +1,4 @@
 from .selector import create_selection, create_total_selection
 from .reducers import Summarizers
 from .tokenizer import tokenize_sentences
+reducers_count = len(Summarizers)
--- prototype_server/app.py
+++ prototype_server/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from random import random
 from core import (
     create_total_selection,
+    reducers_count,
     colors,
     tokenize_sentences
 )
@@ -14,9 +15,10 @@ CORS(app)
 @app.route('/reduce', methods=['POST'])
 def reduce():
     content = request.get_json()
-    if not content.text:
-        raise AttributeError('Please provide text parameter')
-    options = content.sum_options if content.sum_options else {"ratio": 0.5}
-    text_sentences = tokenize_sentences(content.text)
+    if "text" not in content:
+        raise RuntimeError('Please provide text parameter')
+    options = content["sum_options"] if "sum_options" in content else {"ratio": 0.5}
+    text_sentences = tokenize_sentences(content["text"])
     selection = create_total_selection(text_sentences, options)
-    return {"text_sentences": text_sentences, "total_selection": selection}
+
+    return {"text_sentences": text_sentences, "total_selection": selection, "max_selection": reducers_count}
