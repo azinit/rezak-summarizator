27b1fd0a047706f5837266f4cc673b73b560281c
niyaz <niyazm524@yandex.ru>
2020-03-22 16:34:52 +0000

    running server with bjoern on production

--- prototype_server/__init__.py
+++ prototype_server/__init__.py
@@ -1,1 +1,10 @@
+from os import getenv
 from .app import app
+
+
+def run():
+    if getenv('PYTHON_ENV') == 'production':
+        import bjoern
+        bjoern.run(app, '0.0.0.0', 29500)
+    else:
+        app.run('0.0.0.0')
