21047d39d30587ce8cd45f403258f31a1ac361f6
niyazm524 <niyazm524@gmail.com>
2020-03-24 00:07:40 +0300

    modal window added

--- client/chrome/scripts/content.ts
+++ client/chrome/scripts/content.ts
@@ -3,24 +3,36 @@
  */
 
 import messenger from './messengers/content-messenger'
+import Modal from 'modal-js/src/modal'
+import './content.css'
 
 let firedElement: Element = null;
 const colors = ["#757575", "#ccffe7", "#a1ffd3", "#4affaa", "#00ff87", "#00ff87", "#00ff87", "#00ff87"];
 
 messenger.registerHandler('CONTEXT_ACTION_CLICK', (data) => {
     console.log('Clicked element is ', firedElement);
-    if(firedElement) return data;
+    if (firedElement) return data;
 });
 
 messenger.registerHandler('HIGHLIGHT', ({text_sentences, total_selection}) => {
     if (!firedElement) return 'fail';
     console.log('To be highlighted', text_sentences);
-    firedElement.innerHTML = text_sentences.map((sentence, index) =>
+    const html = text_sentences.map((sentence, index) =>
         `<span style="color: ${colors[total_selection[index]]}">${sentence}</span>`
     ).join('\n');
+    let modal = new Modal(`<div class="rezak-modal" style="width: ${firedElement.clientWidth}px; top: ${firedElement.offsetTop}px">
+            ${html}</div>`, {
+        containerEl: firedElement,
+        activeClass: 'modal-active'
+    });
+    modal.show();
     return "completed"
 });
 
 document.addEventListener('contextmenu', (event) => {
-    firedElement = event.target as Element;
+    firedElement = getSelection();
 });
+
+function getSelection(): Element | null {
+    return window.getSelection().anchorNode?.parentElement
+}
